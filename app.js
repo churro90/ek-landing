28e01498a6420a5e02d68cfcc5a09b051a21ee90
Martin Carrasco <martin.carrascof@gmail.com>
2018-05-27 17:51:40 -0400

    yapo wn

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ mongoose.connection.on('error', (err) => {
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
